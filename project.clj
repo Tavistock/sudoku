803ae9d068b549f56a1ae46f98df9f42a7f4ad27
Travis McNeill <tavistock91@gmail.com>
2015-01-06 10:44:23 -0500

    added new build 'lein cljsbuild gh-pages' for gh-pages build

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
+  
   :source-paths ["src/clj" "src/cljs"]
-
+  
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371" :scope "provided"]
                  [ring "1.3.1"]
@@ -19,14 +19,14 @@
                  [leiningen "2.5.0"]
                  ; added by me
                  [ankha "0.1.5.1-21e6ac"]]
-
+  
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-environ "1.0.0"]]
-
+  
   :min-lein-version "2.5.0"
-
+  
   :uberjar-name "sudoku.jar"
-
+  
   :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
                              :compiler {:output-to     "resources/public/js/app.js"
                                         :output-dir    "resources/public/js/out"
@@ -34,21 +34,26 @@
                                         :preamble      ["react/react.min.js"]
                                         :externs       ["react/externs/react.js"]
                                         :optimizations :none
-                                        :pretty-print  true}}}}
-
+                                        :pretty-print  true}}
+                       :gh-pages {:source-paths ["env/prod/cljs"]
+                                  :compiler {:output-to     "js/app.js"
+                                             :preamble      ["react/react.min.js"]
+                                             :optimizations :advanced
+                                             :pretty-print  false}}}}
+  
   :profiles {:dev {:repl-options {:init-ns sudoku.server
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-
+                   
                    :plugins [[lein-figwheel "0.1.4-SNAPSHOT"]]
-
+                   
                    :figwheel {:http-server-root "public"
                               :port 3449
                               :css-dirs ["resources/public/css"]}
-
+                   
                    :env {:is-dev true}
-
+                   
                    :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]}}}}
-
+             
              :uberjar {:hooks [leiningen.cljsbuild]
                        :env {:production true}
                        :omit-source true
